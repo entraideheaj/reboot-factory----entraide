c5303c3cb26e309fda222fb77e8e5c1ab582cfbb
timserck <timserck@gmail>
2015-01-06 20:18:35 +0100

    login

--- _js/js.js
+++ _js/js.js
@@ -3,10 +3,13 @@
 var input_group = $('#group');
 var input_profil = $('#profil');
 var input_search = $('#searchT');
+var input_submit = $('#submit');
 
  var profil = $('#button_profil');
  var group = $('#button_group');
  var search = $('#button_search');
+
+ var page_login = $('.page_login');
 
  profil.click(function(event) {
 
@@ -27,4 +30,10 @@ var input_search = $('#searchT');
             input_profil.attr('checked', false);
             input_group.attr('checked', false);
         });
+
+  input_submit.click(function(event) {
+
+    event.preventDefault();
+    page_login.fadeOut();
+  });
     });
