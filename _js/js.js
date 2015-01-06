16bc2d4c90fe1b18882c4b69502b0a79518ab34e
Kehlian <clement.schmouker@gmail.com>
2015-01-06 17:36:20 +0100

    Manque les délais d'animation

--- _js/js.js
+++ _js/js.js
@@ -2,20 +2,29 @@
 
 var input_group = $('#group');
 var input_profil = $('#profil');
+var input_search = $('#searchT');
 
  var profil = $('#button_profil');
  var group = $('#button_group');
+ var search = $('#button_search');
 
  profil.click(function(event) {
 
 
             input_group.attr('checked', false);
-
+            input_search.attr('checked', false);
     });
 
   group.click(function(event) {
 
             input_profil.attr('checked', false);
+            input_search.attr('checked', false);
         });
 
+
+  search.click(function(event) {
+
+            input_profil.attr('checked', false);
+            input_group.attr('checked', false);
+        });
     });
