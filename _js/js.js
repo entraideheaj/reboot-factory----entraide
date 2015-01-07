f18e53059b6c6d8cfb02f136cc722bd67dc34177
Kehlian <clement.schmouker@gmail.com>
2015-01-07 10:19:29 +0100

    JS function
    new function

--- _js/js.js
+++ _js/js.js
@@ -14,85 +14,69 @@ $(document).ready(function() {
     var span = $('span');
     span.text('Home');
 
-    var bSearchState = true;
-    var bProfilState = true;
-    var bAddState = true;
-    var bGroupState = true;
-
-
-    profil.click(function(event) {
-
-        if (bProfilState == true) {
-
-
+    var bSearchState = false;
+    var bProfilState = false;
+    var bAddState = false;
+    var bGroupState = false;
+    //Function that will check the state of page
+    var iSwitchState = 0;
+    function checkState()
+    {
+        if (iSwitchState == 0) {
+            input_group.attr('checked', false);
+            input_profil.attr('checked', false);
+            input_search.attr('checked', false);
+            span.text('Home');
+        }
+        else if (iSwitchState == 1) {
+            input_profil.attr('checked', true);
             input_group.attr('checked', false);
             input_search.attr('checked', false);
             span.text('Profil');
-            bProfilState = false;
         }
-        else{
-            span.text('Home');
-  
-            bProfilState = true;
-           
-    bAddState = true;
-    bGroupState = true;
-            bSearchState = true;
-
-        }
-
-    });
-
-    group.click(function(event) {
-
-        if (bGroupState == true) {
-
+        else if (iSwitchState == 2) {
             input_profil.attr('checked', false);
+            input_group.attr('checked', true);
             input_search.attr('checked', false);
-            span.text('Group');
-            bGroupState = false;
+            span.text('Groupe');
         }
-        else{
-
-            span.text('Home');
-           
-            bGroupState = true;
-
-    bAddState = true;
-    bGroupState = true;
-    bSearchState = true;
-
-        }
-
-    });
-
-    search.click(function(event) {
-
-        if (bSearchState == true) {
-
+        else if (iSwitchState == 3) {
             input_profil.attr('checked', false);
             input_group.attr('checked', false);
-            span.text('Search');
-            bSearchState = false;
-        }
-        else{
-
-            span.text('Home');
-            
-    
-    bProfilState = true;
-    bAddState = true;
-    bGroupState = true;
-            bSearchState = true;
-            console.log("bla");
+            input_search.attr('checked', true);
+            span.text('Recherche');
         }
 
+        console.log("State Checked");
+    }
+
+    profil.click(function () {
+        if (iSwitchState != 1)
+            iSwitchState = 1;
+        else
+            iSwitchState = 0;
+        checkState();
+    });
+    group.click(function () {
+        if (iSwitchState != 2)
+            iSwitchState = 2;
+        else
+            iSwitchState = 0;
+        checkState();
+    })
+    search.click(function () {
+        if (iSwitchState != 3)
+            iSwitchState = 3;
+        else
+            iSwitchState = 0;
+        checkState();
+    })
+
+    input_submit.click(function (event) {
+        event.preventDefault();
+        page_login.fadeOut();
     });
 
-            input_submit.click(function(event) {
-            event.preventDefault();
-            page_login.fadeOut();
-    });
 
 
 });
