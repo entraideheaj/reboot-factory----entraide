45a0c5c2855ba22f27a2c5d7455f89fc395f09d1
Kehlian <clement.schmouker@gmail.com>
2015-01-07 10:51:33 +0100

    Javascript
    Bugged function

--- _js/js.js
+++ _js/js.js
@@ -11,67 +11,73 @@ $(document).ready(function() {
 
     var page_login = $('.page_login');
 
-    var span = $('span');
-    span.text('Home');
+    var sText = $('.header__div__span');
+    sText.text('Home');
 
     var bSearchState = false;
     var bProfilState = false;
     var bAddState = false;
     var bGroupState = false;
+
     //Function that will check the state of page
     var iSwitchState = 0;
-    function checkState()
-    {
-        if (iSwitchState == 0) {
-            input_group.attr('checked', false);
-            input_profil.attr('checked', false);
-            input_search.attr('checked', false);
-            span.text('Home');
-        }
-        else if (iSwitchState == 1) {
-            input_profil.attr('checked', true);
-            input_group.attr('checked', false);
-            input_search.attr('checked', false);
-            span.text('Profil');
-        }
-        else if (iSwitchState == 2) {
-            input_profil.attr('checked', false);
-            input_group.attr('checked', true);
-            input_search.attr('checked', false);
-            span.text('Groupe');
-        }
-        else if (iSwitchState == 3) {
-            input_profil.attr('checked', false);
-            input_group.attr('checked', false);
-            input_search.attr('checked', true);
-            span.text('Recherche');
-        }
-
-        console.log("State Checked");
+    function checkState() {
+        switch (iSwitchState) {
+            case 0:
+                input_group.attr('checked', false);
+                input_profil.attr('checked', false);
+                input_search.attr('checked', false);
+                sText.text('Home');
+                break;
+            case 1:
+                input_profil.attr('checked', true);
+                input_group.attr('checked', false);
+                input_search.attr('checked', false);
+                sText.text('Profil');
+                break;
+            case 2:
+                input_profil.attr('checked', false);
+                input_group.attr('checked', true);
+                input_search.attr('checked', false);
+                sText.text('Groupe');
+                break;
+            case 3:
+                input_profil.attr('checked', false);
+                input_group.attr('checked', false);
+                input_search.attr('checked', true);
+                sText.text('Recherche');
+                break;
+        };
     }
-
-    profil.click(function () {
-        if (iSwitchState != 1)
+    //For each button: change the state
+    profil.click(function () { //Profil
+        if (iSwitchState != 1) {
             iSwitchState = 1;
-        else
+        }
+        else {
             iSwitchState = 0;
+        }
         checkState();
     });
-    group.click(function () {
-        if (iSwitchState != 2)
+    group.click(function () { //Group
+        if (iSwitchState != 2) {
             iSwitchState = 2;
-        else
+        }
+        else {
             iSwitchState = 0;
+        }
         checkState();
     })
-    search.click(function () {
-        if (iSwitchState != 3)
+    search.click(function () { //Search
+        if (iSwitchState != 3) {
             iSwitchState = 3;
-        else
+        }
+        else {
             iSwitchState = 0;
+        }
         checkState();
     })
-
+    //Fade out the login Page
     input_submit.click(function (event) {
         event.preventDefault();
         page_login.fadeOut();
