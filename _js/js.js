8bef02bbec1102c29d69ae20fb2079f9b64783d1
timserck <timserck@gmail>
2015-01-07 13:30:07 +0100

    select

--- _js/js.js
+++ _js/js.js
@@ -88,6 +88,43 @@ a.next("div").hide();
         }
 
     });
+
+    //slide
+    var upButton = $(".selectUp");
+var downButton = $(".selectDown");
+var groups = $(".allGroups");
+var iMoveIndex = 1;
+var iNumberOfGroups = 8;
+
+
+    upButton.click(animateUp);
+    downButton.click(animateDown);
+
+
+function animateDown()
+{
+    if (iMoveIndex <= iNumberOfGroups - 3)//Number of groups minus the number of group displayed
+    {
+        groups.animate(
+            { marginTop: "-=52px" },
+            200
+            );
+        iMoveIndex += 1;
+    }
+    console.log(iMoveIndex);
+}
+
+function animateUp()
+{
+    if (iMoveIndex >= iNumberOfGroups-6)//Number of groups minus the number of group gone through
+    {
+        groups.animate(
+            { marginTop: "+=52px" },
+            200
+            );
+        iMoveIndex -= 1;
+    }
+}
  
 
 });
