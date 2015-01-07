7ed28e382e61e5eec57b8c709604835dc67ac1f6
timserck <timserck@gmail>
2015-01-07 09:02:20 +0100

    text span

--- _js/js.js
+++ _js/js.js
@@ -1,47 +1,45 @@
- $(document).ready(function() {
+$(document).ready(function() {
 
-var input_group = $('#group');
-var input_profil = $('#profil');
-var input_search = $('#searchT');
-var input_submit = $('#submit');
+    var input_group = $('#group');
+    var input_profil = $('#profil');
+    var input_search = $('#searchT');
+    var input_submit = $('#submit');
 
- var profil = $('#button_profil');
- var group = $('#button_group');
- var search = $('#button_search');
+    var profil = $('#button_profil');
+    var group = $('#button_group');
+    var search = $('#button_search');
 
- var page_login = $('.page_login');
+    var page_login = $('.page_login');
 
- var span =$('span');
-span.text('Home');
+    var span =$('span');
+    span.text('Home');
 
- profil.click(function(event) {
 
+    profil.click(function(event) {
 
             input_group.attr('checked', false);
             input_search.attr('checked', false);
             span.text('Profil');
     });
 
-  group.click(function(event) {
+    group.click(function(event) {
 
             input_profil.attr('checked', false);
             input_search.attr('checked', false);
             span.text('Group');
 
-        });
+    });
 
-
-  search.click(function(event) {
+    search.click(function(event) {
 
             input_profil.attr('checked', false);
             input_group.attr('checked', false);
             span.text('Search');
 
-        });
-
-  input_submit.click(function(event) {
-
-    event.preventDefault();
-    page_login.fadeOut();
-  });
     });
+
+            input_submit.click(function(event) {
+            event.preventDefault();
+            page_login.fadeOut();
+    });
+});
