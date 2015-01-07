b9fe7efe1cc6d67cee4cfd303bd7e026c01a1de6
timserck <timserck@gmail>
2015-01-07 17:06:33 +0100

    tim fin

--- _js/js.js
+++ _js/js.js
@@ -89,41 +89,10 @@ a.next("div").hide();
 
     });
 
-    //slide
-    var upButton = $(".selectUp");
-var downButton = $(".selectDown");
-var groups = $(".allGroups");
-var iMoveIndex = 1;
-var iNumberOfGroups = 8;
+ $('cour').click(function(event){
 
-
-    upButton.click(animateUp);
-    downButton.click(animateDown);
-
-
-function animateDown()
-{
-    if (iMoveIndex <= iNumberOfGroups - 3)//Number of groups minus the number of group displayed
-    {
-        groups.animate(
-            { marginTop: "-=52px" },
-            200
-            );
-        iMoveIndex += 1;
-    }
-}
-
-function animateUp()
-{
-    if (iMoveIndex >= iNumberOfGroups-6)//Number of groups minus the number of group gone through
-    {
-        groups.animate(
-            { marginTop: "+=52px" },
-            200
-            );
-        iMoveIndex -= 1;
-    }
-}
+    
+ });
  
 
 });
