caff9f24072473a893e27041cfda14864ed14874
timserck <timserck@gmail>
2015-01-07 09:07:45 +0100

    span text review

--- _js/js.js
+++ _js/js.js
@@ -11,30 +11,61 @@ $(document).ready(function() {
 
     var page_login = $('.page_login');
 
+    var etat = true;
     var span =$('span');
     span.text('Home');
 
 
     profil.click(function(event) {
 
+        if (etat == true) {
+
+
             input_group.attr('checked', false);
             input_search.attr('checked', false);
             span.text('Profil');
+            etat = false;
+        }
+        else{
+            span.text('Home');
+            etat = true;
+
+        }
+
     });
 
     group.click(function(event) {
 
+        if (etat == true) {
+
             input_profil.attr('checked', false);
             input_search.attr('checked', false);
             span.text('Group');
+            etat = false;
+        }
+        else{
+
+            span.text('Home');
+            etat = true;
+
+        }
 
     });
 
     search.click(function(event) {
 
+        if (etat == true) {
+
             input_profil.attr('checked', false);
             input_group.attr('checked', false);
             span.text('Search');
+            etat = false;
+        }
+        else{
+
+            span.text('Home');
+            etat = true;
+        }
 
     });
 
